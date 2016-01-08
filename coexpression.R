7dd55a3aa7f923fa3d8d735c5ca38f31d3f7d362
jonwells90 <jonwells90@gmail.com>
2016-01-08 14:52:18 +0000

    change coexpression

--- coexpression.R
+++ coexpression.R
@@ -1,6 +1,7 @@
 library("ggplot2")
 library("dplyr")
 
+ned_and_ed <- read.table("halflife/halflife/data/NEDandED_subunit_comps.txt")$V1
 df_corum <- read.table("halflife/halflife/data/coexpressdb_corum_combined.tsv")
 colnames(df_corum) <- c("complex.id", "entrez.id", "uniprot.id", 
                         "avg.coexpression", "decay.class", "species")
@@ -11,12 +12,24 @@ density_plotter <- function(df){
   plt <- ggplot(df, aes(x = avg.coexpression, fill = decay.class)) +
     geom_density(alpha = 0.7) + 
     scale_fill_manual(values = c("cadetblue4", "darkgoldenrod1", "darkred")) +
-    xlab("Average coexpression (Pearson's") +
+    xlab("Average coexpression (Pearson's r)") +
     ylab("Density") +
     labs(fill = "Decay class")
   return(plt)
 }
 density_plotter(df_corum)
+
+bin_plotter <- function(df, interval){
+  df_counts <- count(df, complex.id)
+  df_counts <- mutate(df_counts, 
+                      intervals = findInterval(df_counts$n, 
+                                               seq(1, max(df_counts$n), 5)))
+  comps <- filter(df_counts, intervals == interval)$complex.id
+  df_bin <- filter(df, complex.id %in% comps)
+  plt <- density_plotter(df_bin)  
+  return(plt)
+}
+bin_plotter(df_corum, 4)
 
 box_plotter <- function(df){
   # df <- filter(df, !is.na(decay.class))

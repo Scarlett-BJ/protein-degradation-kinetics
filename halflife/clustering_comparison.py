11ddb8f2f393210b5a8dd695884d126c4d692f13
jonwells90 <jonwells90@gmail.com>
2016-01-08 14:51:00 +0000

    deleted a few files

--- halflife/clustering_comparison.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-with open('test.tmp') as f1, open('test1.tmp') as f2:
-    d1 = sorted([set(l.strip().split('\t')[1].split(', ')) for l in f1])
-    d2 = sorted([set(l.strip().split('\t')[1].split(', ')) for l in f2])
-for i in d1:
-    if i not in d2:
-        print(d1[d1.index(i)], d2[d2.index(i)])

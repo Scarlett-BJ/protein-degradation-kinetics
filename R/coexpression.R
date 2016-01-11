9486972ae5567ef39379612d7112413934cd1756
jonwells90 <jonwells90@gmail.com>
2016-01-11 17:15:41 +0000

    delete deprecated data

--- R/coexpression.R
+++ R/coexpression.R
@@ -1,16 +1,15 @@
 library("ggplot2")
 library("dplyr")
 
-ned_and_ed <- read.table("halflife/halflife/data/NEDandED_subunit_comps.txt")$V1
 df_corum <- read.table("halflife/halflife/data/coexpressdb_corum_combined.tsv")
-colnames(df_corum) <- c("complex.id", "entrez.id", "uniprot.id", 
+colnames(df_corum) <- c("complex.id", "entrez.id", "uniprot.id",
                         "avg.coexpression", "decay.class", "species")
 
 density_plotter <- function(df){
   df <- filter(df, !is.na(decay.class))
   df$decay.class <- factor(df$decay.class, levels = c("UN", "ED", "NED"))
   plt <- ggplot(df, aes(x = avg.coexpression, fill = decay.class)) +
-    geom_density(alpha = 0.7) + 
+    geom_density(alpha = 0.7) +
     scale_fill_manual(values = c("cadetblue4", "darkgoldenrod1", "darkred")) +
     xlab("Average coexpression (Pearson's r)") +
     ylab("Density") +
@@ -21,12 +20,12 @@ density_plotter(df_corum)
 
 bin_plotter <- function(df, interval){
   df_counts <- count(df, complex.id)
-  df_counts <- mutate(df_counts, 
-                      intervals = findInterval(df_counts$n, 
+  df_counts <- mutate(df_counts,
+                      intervals = findInterval(df_counts$n,
                                                seq(1, max(df_counts$n), 3)))
   comps <- filter(df_counts, intervals == interval)$complex.id
   df_bin <- filter(df, complex.id %in% comps)
-  plt <- density_plotter(df_bin)  
+  plt <- density_plotter(df_bin)
   return(plt)
 }
 bin_plotter(df_corum, 10)
@@ -34,9 +33,9 @@ bin_plotter(df_corum, 10)
 box_plotter <- function(df){
   # df <- filter(df, !is.na(decay.class))
   df$decay.class <- factor(df$decay.class, levels = c("NED", "UN", "ED"))
-  plt <- ggplot(df, aes(x = decay.class, y = avg.coexpression, 
+  plt <- ggplot(df, aes(x = decay.class, y = avg.coexpression,
                         fill = decay.class)) +
-    geom_boxplot(notch = TRUE, outlier.size = 0.75) + 
+    geom_boxplot(notch = TRUE, outlier.size = 0.75) +
     scale_fill_manual(values = c("darkred", "cadetblue4", "darkgoldenrod1")) +
     xlab("Decay class") +
     ylab("Average coexpression (Pearson's r)") +
